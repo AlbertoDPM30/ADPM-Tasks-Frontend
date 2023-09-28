cee9e3af2daad597d550c21c7e0ebc9265ca3d2a
AlbertoDPM30 <albertodpm2000@gmail.com>
2023-09-28 04:00:17 -0400

    Modificacion a la URL de la api axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   //baseURL: "http://localhost:3000/api",
-  baseURL: "https://main.dlxa3ezjjvopg.amplifyapp.com/",
+  baseURL: "https://main.dlxa3ezjjvopg.amplifyapp.com/api",
   withCredentials: true,
 });
 
