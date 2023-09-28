db52ed16a2299fa7e46388431e3d3cd8a6ff48ff
AlbertoDPM30 <albertodpm2000@gmail.com>
2023-09-28 02:58:27 -0400

    Ruta al servidor en linea

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  //baseURL: "http://localhost:3000/api",
+  baseURL: "https://main.dlxa3ezjjvopg.amplifyapp.com/",
   withCredentials: true,
 });
 
