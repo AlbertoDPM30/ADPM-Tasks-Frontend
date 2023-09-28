6f6ad4d131476cce561f238e66ba26fc4bae2cec
AlbertoDPM30 <albertodpm2000@gmail.com>
2023-09-28 07:58:57 -0400

    nuevo destino URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   //baseURL: "http://localhost:3000/api",
-  baseURL: "https://main.dlxa3ezjjvopg.amplifyapp.com/api",
+  //baseURL: "https://main.dlxa3ezjjvopg.amplifyapp.com/api",
+  baseURL: "http://3.20.40.119/api",
   withCredentials: true,
 });
 
