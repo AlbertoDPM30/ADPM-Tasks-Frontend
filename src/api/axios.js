f90710f95a3809b9c1f7e03c466c748f3b51c731
AlbertoDPM30 <albertodpm2000@gmail.com>
2023-09-30 20:22:19 -0400

    baseURL: https://adpmtasksbackend.onrender.com/api

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   //baseURL: "http://localhost:3000/api",
   //baseURL: "https://main.dlxa3ezjjvopg.amplifyapp.com/api",
-  baseURL: "http://adpmtasksbackend/api",
+  baseURL: "https://adpmtasksbackend.onrender.com/api",
   withCredentials: true,
 });
 
